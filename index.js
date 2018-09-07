9391bf785906f1b70e3d173ba89171f796ad4ca2
Ruslan D <drumm2k@gmail.com>
2018-09-07 20:15:33 +0300

    refactor. Moved part of logic from index.js to weather.js

--- index.js
+++ index.js
@@ -4,65 +4,35 @@ const weather = require('./weather.js');
 const geocoding = require('./geocoding.js');
 
 const bot = new Telegraf(config.botToken);
+bot.startPolling();
 
 bot.hears(/Всем привет/i, (ctx) => ctx.reply('Это Джи!'));
 
-bot.startPolling();
-
 // Weather
-const conditionDict = {
-  'clear': '☀️',
-  'partly-cloudy': '⛅️',
-  'cloudy': '☁️',
-  'overcast': '🌥',
-  'partly-cloudy-and-light-rain': '🌦',
-  'partly-cloudy-and-rain': '🌦',
-  'overcast-and-rain': '🌧',
-  'overcast-thunderstorms-with-rain': '⛈',
-  'cloudy-and-light-rain': '🌧',
-  'overcast-and-light-rain': '🌧',
-  'cloudy-and-rain': '🌧',
-  'overcast-and-wet-snow': '🌧🌨',
-  'partly-cloudy-and-light-snow': '🌨',
-  'partly-cloudy-and-snow': '🌨',
-  'overcast-and-snow': '🌨',
-  'cloudy-and-light-snow': '🌨',
-  'overcast-and-light-snow': '🌨',
-  'cloudy-and-snow': '🌨'
-};
-
 bot.hears(/weather/i, (ctx) => {
-  // Determine location
-  // Clean message by removing command, you could change @keanureeves_bot to your bot name
+  // Handle input message by removing command
   let location = ctx.update.message.text.toLowerCase().replace(/\/|weather| /gy, '');
 
-  // If location is blank use default
+  // If location is not set - use default
   if (location === '') {
     location = config.weatherDefaultLocation;
   }
 
-  // Fetch coordinates based on location
+  // Use Geocoding API to get true location name and coordinates
   geocoding.fetchCoordinates(location).then(response => {
     if (response === undefined) {
       ctx.reply('🚫 The Matrix has you.');
       return;
     }
 
-    // Set name and coords
+    // Set name and coords returned by Geocoding API
     let location = response[0];
     let lon = response[1];
     let lat = response[2];
 
-    // Fetch weather for location
-    weather.fetchWeather(lat, lon).then(response => {
-      ctx.reply(`🚩 ${location}:` +
-                `\n🌡 ${response.fact.temp}°C` +
-                ` ${conditionDict[response.fact.condition]} ${response.fact.condition.replace(/-/g, ' ')}` +
-                `\n🌬 ${response.fact.wind_speed}m/s 💧 ${response.fact.humidity}%` +
-                `\n🌅 ${response.forecasts[0].sunrise} 🌇 ${response.forecasts[0].sunset}` +
-                `\n` +
-                `\n🗓️ Tomorrow: ` +
-                `\n🌡 ${response.forecasts[1].parts.night_short.temp} - ${response.forecasts[1].parts.day_short.temp} °C`);
+    // Use Weather API to get forecast
+    weather.getWeather(location, lat, lon).then(response => {
+      ctx.reply(response);
     });
   });
 });
--- weather.js
+++ weather.js
@@ -1,7 +1,45 @@
 const fetch = require('node-fetch');
 const config = require('./config.json');
 
-module.exports.fetchWeather = function(lat, lon) {
+// Dictionary for conditions
+const conditionDict = {
+  'clear': '☀️',
+  'partly-cloudy': '⛅️',
+  'cloudy': '☁️',
+  'overcast': '🌥',
+  'partly-cloudy-and-light-rain': '🌦',
+  'partly-cloudy-and-rain': '🌦',
+  'overcast-and-rain': '🌧',
+  'overcast-thunderstorms-with-rain': '⛈',
+  'cloudy-and-light-rain': '🌧',
+  'overcast-and-light-rain': '🌧',
+  'cloudy-and-rain': '🌧',
+  'overcast-and-wet-snow': '🌧🌨',
+  'partly-cloudy-and-light-snow': '🌨',
+  'partly-cloudy-and-snow': '🌨',
+  'overcast-and-snow': '🌨',
+  'cloudy-and-light-snow': '🌨',
+  'overcast-and-light-snow': '🌨',
+  'cloudy-and-snow': '🌨'
+};
+
+// Get weather and form reply
+module.exports.getWeather = function(location, lat, lon) {
+  return fetchWeather(lat, lon).then(response => {
+    let wMarkup = (`🚩 ${location}:` +
+                  `\n🌡 ${response.fact.temp}°C` +
+                  ` ${conditionDict[response.fact.condition]} ${response.fact.condition.replace(/-/g, ' ')}` +
+                  `\n🌬 ${response.fact.wind_speed}m/s 💧 ${response.fact.humidity}%` +
+                  `\n🌅 ${response.forecasts[0].sunrise} 🌇 ${response.forecasts[0].sunset}` +
+                  `\n` +
+                  `\n🗓️ Tomorrow: ` +
+                  `\n🌡 ${response.forecasts[1].parts.night_short.temp} - ${response.forecasts[1].parts.day_short.temp} °C`);
+    return wMarkup;
+  });
+};
+
+// Fetch weather
+function fetchWeather(lat, lon) {
   return fetch(`https://api.weather.yandex.ru/v1/forecast?lat=${lat}&lon=${lon}`, {
     headers: {'X-Yandex-API-Key': config.weatherYandexAPI}
   }).then(response => response.json())
