25ed4adcb638ecc0bc9c543163e25b9c1e08537f
Ruslan D <drumm2k@gmail.com>
2018-09-07 18:39:54 +0300

    fix. Removed response check, there is catch for errors

--- geocoding.js
+++ geocoding.js
@@ -2,7 +2,6 @@ const fetch = require('node-fetch');
 
 module.exports.fetchCoordinates = function(location) {
   return fetch(`https://geocode-maps.yandex.ru/1.x/?format=json&geocode=${location}&lang=en_US`)
-    .then(checkGeoStatus)
     .then(response => response.json())
     .then(json => {
       const jsonPath = json.response.GeoObjectCollection.featureMember[0];
@@ -22,11 +21,3 @@ module.exports.fetchCoordinates = function(location) {
       console.log('Geolocation fetch error: ' + error);
     });
 };
-
-function checkGeoStatus(response) {
-  if (response.ok) {
-    return response;
-  } else {
-    throw console.log('Error Geocoding API response: ' + response.statusText);
-  }
-}
--- weather.js
+++ weather.js
@@ -4,8 +4,7 @@ const config = require('./config.json');
 module.exports.fetchWeather = function(lat, lon) {
   return fetch(`https://api.weather.yandex.ru/v1/forecast?lat=${lat}&lon=${lon}`, {
     headers: {'X-Yandex-API-Key': config.weatherYandexAPI}
-  }).then(checkWeatherStatus)
-    .then(response => response.json())
+  }).then(response => response.json())
     .then(json => {
       return json;
     })
@@ -13,11 +12,3 @@ module.exports.fetchWeather = function(lat, lon) {
       console.log('Weather fetch error: ' + error);
     });
 };
-
-function checkWeatherStatus(response) {
-  if (response.ok) {
-    return response;
-  } else {
-    throw console.log('Error Weather API response: ' + response.statusText);
-  }
-}
